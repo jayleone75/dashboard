0679ca4b70088c6f7d04363ebc319f1633340228
jayleone75 <mail@jayleone.com>
2026-06-10 09:27:25 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // force rebuild
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
+  define: { __BUILD__: Date.now() }
 });
-
